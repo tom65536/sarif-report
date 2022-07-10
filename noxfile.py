59879c4bdddb6dbbe68c03c0ff917409bb3d2672
tom65536 <48080661+tom65536@users.noreply.github.com>
2022-07-10 22:33:32 +0200

    Adds schema file, fixes linter issues

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,33 @@
+"""Sessions for nox."""
+import pathlib
 
 import nox
+
+
+TARGET_PATH = pathlib.Path("target")
+LINT_REPORT_PATH = TARGET_PATH / "lint"
 
 
 @nox.session
 def test(ctx: nox.Session) -> None:
     """Run unit tests."""
-    ctx.install('pytest', 'pytest-click', '.')
-    ctx.run('pytest')
+    ctx.install("pytest", "pytest-click", ".")
+    ctx.run("pytest")
+
+
+@nox.session
+def lint(ctx: nox.Session) -> None:
+    """Run static code analysis."""
+    LINT_REPORT_PATH.mkdir(exist_ok=True, parents=True)
+    ctx.install(
+        "pylama[all]",
+    )
+
+    args = [
+        "--report",
+        f'{LINT_REPORT_PATH/"lint-report.json"}',
+    ]
+    if ctx.posargs:
+        args += ctx.posargs
+
+    ctx.run("pylama", *args)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,4 @@
-
-import pytest
-
+"""Test cases for the main program."""
 import sarif_report.__main__ as main
 
 
@@ -8,4 +6,4 @@ def test_main(cli_runner) -> None:
     """Run the main program."""
     result = cli_runner.invoke(main.hello, [])
     assert result.exit_code == 0
-    assert result.output.strip() == 'Hello there'
+    assert result.output.strip() == "Hello there"
